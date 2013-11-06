08985c34d6c4a28a960d1266540e2dad93fd02bc
Cliffano Subagio <cliffano@gmail.com>
2013-11-06 18:42:28 +1100

    Add integration test to CI run.

---
+++ test-integration/sapi.js
@@ -0,0 +1,18 @@
+var buster = require('buster-node'),
+  referee = require('referee'),
+  Sapi = require('../lib/sapi'),
+  assert = referee.assert;
+
+buster.testCase('sapi - sapi', {
+  setUp: function () {
+    this.timeout = 5000;
+  },
+  'should error when API key is invalid': function (done) {
+    var sapi = new Sapi('someinvalidapikey');
+    sapi.query('italian').location('melbourne').search(function (err, result) {
+      assert.equals(err.message, 'Authentication failed -  invalid key someinvalidapikey');
+      assert.equals(result, undefined);
+      done();
+    });
+  }
+});
